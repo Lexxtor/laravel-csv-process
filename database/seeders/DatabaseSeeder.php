2f22954c150c1108c7f3ee64b19b8a328cdbae29
Lexxtor <turakod@gmail.com>
2021-01-22 01:06:50 +0700

    clean up

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         \App\Models\User::factory(10)->create();
          \App\Models\Service::factory(100000)->create();
     }
 }
